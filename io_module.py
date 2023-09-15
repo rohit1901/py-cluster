b8bce6743195e9b639bb27b76a0158d5aff39792
Rohit Khanduri <rohit.khanduri@hotmail.com>
2023-09-15 16:32:21 +0200

    feat: read 3 files for red, green, and unknown data sets
    updated io_module.py

--- io_module.py
+++ io_module.py
@@ -4,10 +4,12 @@ def read_multi_dim_data(filename):
     """
     Reads a multi-dimensional data from a file.
     """
-    dataset =[]
-
-    ##from tutorial
-
+    dataset = []
+    with open(filename, 'r') as f:
+        for line in f:
+            data = line.split()
+            dataset.append([float(x) for x in data])
+    print(dataset)
     return dataset
 
 
